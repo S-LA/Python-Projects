a503dc5dbd5dfce019bc3ce0d88be1e9e81e2e84
teacher <54679729+S-LA@users.noreply.github.com>
2019-10-20 01:18:50 +0200

    Well done!!!
    I am very impressed, I had a look at it and the only issue I found was that because you called the "Roll_1, _2 and _3" at the top of the code the result remained the same, I moved them into the if functions so it does a new random calculation every time each one is called.
    
    I also saw you called them roll_1 roll_2 etc, its not a thing but I prefer using letters like A, B and C so that you have only letters for names and numbers for maths.

--- dice project.py
+++ dice project.py
@@ -1,36 +1,34 @@
 import random
 import winsound
 
-Roll1 = random.sample(range(1,7),1)
-Roll2 = random.sample(range(1,7),2)
-Roll3 = random.sample(range(1,7),3)
 
 loop = True
 while loop == True:
-    print("----------------------")
+    print("\n---------------------------------")
     print("Please choose between 1 or 2 or 3")
-    option = str(input("Select number of dice:"))
+    option = str(input("Select number of dice:\n"))
 
     if option == "1":
-        print(Roll1)
-        winsound.PlaySound("dice.wav", winsound.SND_FILENAME)
+        Roll_A = random.sample(range(1,7),1)
+        print("\t" + str(Roll_A))
+        print ("\t total: "+ str(Roll_A))
+        winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
         print("Next Turn")
         
 
     if option == "2":
-        print(Roll2)
-        winsound.PlaySound("dice.wav", winsound.SND_FILENAME)
+        Roll_B = random.sample(range(1,7),2)
+        print("\t" + str(Roll_B))
+        sum_B = int(Roll_B[0])+ int(Roll_B[1])
+        print ('\t total: '+ str(sum_B))
+        winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
         print("Next Turn")
        
 
     if option == "3":
-        print(Roll3)
-        winsound.PlaySound("dice.wav", winsound.SND_FILENAME)
+        Roll_C = random.sample(range(1,7),3)
+        print("\t" + str(Roll_C))
+        sum3 = int(Roll_C[0])+ int(Roll_C[1])+int(Roll_C[2])
+        print("\t total: "+ str(sum3))
+        winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
         print("Next Turn")
-        
-
-
-
-
-
-
